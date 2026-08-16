0e2d753b614398a11b5abdd862afb710abd1301d
Samiksha Rajan <your.email@example.com>
2026-08-16 16:43:39 +0530

    Completed Java basics input and variables practice

---
+++ Java-Basics/StudentInfo.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class StudentInfo {
+    public static void main (String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter your name: ");
+        String name = sc.nextLine();
+        System.out.println("Enter your age: ");
+        int age = sc.nextInt();
+        System.out.println("Enter your percentage: ");
+        double percentage = sc.nextDouble();
+
+        System.out.println("\nName: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("Percentage: " + percentage);
+    }
+}
---
+++ Java-Basics/Variables.java
@@ -0,0 +1,13 @@
+public class Variables{
+    public static void main (String[] args){
+        int age = 20;
+        double percentage = 83.5;
+        char grade = 'A';
+        boolean isStudent = true;
+
+        System.out.println(age);
+        System.out.println(percentage);
+        System.out.println(grade);
+        System.out.println(isStudent);
+    }
+}
