4573e50f467bf2ef6b569948f1b8e269b210573b
Samiksha Rajan <your.email@example.com>
2026-08-21 23:16:15 +0530

    Practiced digit counting and number reversal

---
+++ Java-Basics/CountDigits.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class CountDigits{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number: ");
+        int number = sc.nextInt();
+        int count = 0;
+
+        if (number == 0){
+            count = 1;
+        }else{
+            while (number != 0){
+                number = number / 10;
+                count++;
+            }
+        }
+        System.out.println("Count of digits: " + count);
+    }
+}
