c426054426087b3296403f4a1c62e07d6b0b10a0
Samiksha Rajan <your.email@example.com>
2026-08-23 01:25:20 +0530

    Practiced digit sum and odd digit counting

---
+++ Java-Basics/CountOddDigits.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class CountOddDigits{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number: ");
+        int number = sc.nextInt();
+        int count = 0;
+        while(number != 0){
+            int digit = number % 10;
+            if(digit % 2 != 0){
+                count++;
+            }
+            number /= 10;
+        }
+        System.out.println("Count of odd digits: " + count);
+    }
+}
