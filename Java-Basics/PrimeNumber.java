a1df61e2bd3fe1db6b33578dd44df68f73c26a26
Samiksha Rajan <your.email@example.com>
2026-08-24 22:23:19 +0530

    Practiced factors and prime numbers

---
+++ Java-Basics/PrimeNumber.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class PrimeNumber{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number: ");
+        int number = sc.nextInt();
+        int count = 0;
+        for(int i = 1; i <= number; i++){
+            if(number % i == 0){
+                count++;
+            }
+        }
+        if(count == 2){
+            System.out.println(number + " is a prime number.");
+        }else{
+            System.out.println(number + " is not a prime number.");
+        }
+    }
+}
