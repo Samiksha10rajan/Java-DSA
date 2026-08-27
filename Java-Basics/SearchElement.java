8e9168f8858a036bdf9320a5a5d50fc87532318e
Samiksha Rajan <your.email@example.com>
2026-08-27 23:45:43 +0530

    Practiced array searching

---
+++ Java-Basics/SearchElement.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class SearchElement{
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        int[] numbers = {10, 20, 30, 40, 50};
+        System.out.println("Enter the number: ");
+        int target = scanner.nextInt();
+        
+        for(int i = 0; i < numbers.length; i++){
+            if(numbers[i] == target){
+                System.out.println("Element is at index: " + i);
+            }
+        }
+    }
+}
